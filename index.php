9ce1c3577ab34484397b198208df539b2b98a35d
ckondek <chef@loanshark.de>
2019-11-15 15:12:06 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>"Welcome to ExhaustLab"</title>
+  </head>
+
+  <body>
+    <p>ohh the re is so much data out there, every day I make more,<br>
+      and I don't know where it goes
+    </p>
+  </body>
+</html>
